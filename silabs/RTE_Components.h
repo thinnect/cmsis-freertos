ebef597bfd77e365ad6b5ff7a1272bcf389dd00a
Raido Pahtma <raidopahtma@gmail.com>
2019-06-13 14:15:36 +0300

    Make RTE_Components universal for silabs.

--- silabs/RTE_Components.h
+++ silabs/RTE_Components.h
@@ -1,27 +1,22 @@
-
 /*
- * Auto generated Run-Time-Environment Component Configuration File
- *      *** Do not modify ! ***
+ * Run-Time-Environment Component Configuration File
  *
- * Project: 'Blinky' 
- * Target:  'Simulation' 
+ * Hand-crafted, based on something auto-generated.
  */
-
 #ifndef RTE_COMPONENTS_H
 #define RTE_COMPONENTS_H
 
-
 /*
- * Define the Device Header File: 
+ * Define the Device Header File:
  */
-#define CMSIS_device_header "efr32mg12p332f1024gl125.h"
+#define CMSIS_device_header "em_device.h"
 
-#define __ARM_ARCH_7EM__        1
+#define __ARM_ARCH_7EM__                1
 
 #define RTE_CMSIS_RTOS2                 /* CMSIS-RTOS2 */
-        #define RTE_CMSIS_RTOS2_FreeRTOS        /* CMSIS-RTOS2 FreeRTOS */
+#define RTE_CMSIS_RTOS2_FreeRTOS        /* CMSIS-RTOS2 FreeRTOS */
 //#define RTE_Compiler_EventRecorder
-          #define RTE_Compiler_EventRecorder_DAP
+#define RTE_Compiler_EventRecorder_DAP
 #define RTE_RTOS_FreeRTOS_CONFIG_RTOS2  /* RTOS FreeRTOS Config for CMSIS RTOS2 API */
 #define RTE_RTOS_FreeRTOS_CORE          /* RTOS FreeRTOS Core */
 #define RTE_RTOS_FreeRTOS_EVENTGROUPS   /* RTOS FreeRTOS Event Groups */
